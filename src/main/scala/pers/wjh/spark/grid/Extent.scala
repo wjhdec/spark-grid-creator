eae2d23d6a318c8c392a8a57ca1c604e9bc3f0c8
wjhdec <wjhdec@163.com>
2019-11-16 14:42:36 +0800

    feat: 添加ID及计算规则
    ID可以记录层数，后期也可依据这个分区

--- src/main/scala/pers/wjh/spark/grid/Extent.scala
+++ src/main/scala/pers/wjh/spark/grid/Extent.scala
@@ -2,17 +2,18 @@ package pers.wjh.spark.grid
 
 /**
  * 范围
+ * @param id 标识
  * @param xMin 左侧边界
  * @param yMin 下侧边界
  * @param xMax 右侧边界
  * @param yMax 上侧边界
  */
-case class Extent(xMin: Double, yMin: Double, xMax: Double, yMax: Double) {
+case class Extent(id:String, xMin: Double, yMin: Double, xMax: Double, yMax: Double) {
   def separate(xCount: Int, yCount: Int): Seq[Extent] = {
     val xSep = Extent.separate(xMin, xMax, xCount)
     val ySep = Extent.separate(yMin, yMax, yCount)
     for(x <- 0 until xSep.length - 1; y <- 0 until ySep.length - 1) yield {
-      Extent(xSep(x), ySep(y), xSep(x + 1), ySep(y + 1))
+      Extent(s"$id-${x * (ySep.length - 1) + y}",xSep(x), ySep(y), xSep(x + 1), ySep(y + 1))
     }
   }
 }
--- src/test/scala/pers/wjh/spark/grid/ExtentTest.scala
+++ src/test/scala/pers/wjh/spark/grid/ExtentTest.scala
@@ -9,13 +9,22 @@ class ExtentTest extends Specification {
     }
 
     "分离Extent" in {
-      Extent(5, 2, 10, 4).separate(5, 2) must containTheSameElementsAs(
-        Seq(Extent(5.0, 2.0, 6.0, 3.0), Extent(5.0, 3.0, 6.0, 4.0),
-          Extent(6.0, 2.0, 7.0, 3.0), Extent(6.0, 3.0, 7.0, 4.0),
-          Extent(7.0, 2.0, 8.0, 3.0), Extent(7.0, 3.0, 8.0, 4.0),
-          Extent(8.0, 2.0, 9.0, 3.0), Extent(8.0, 3.0, 9.0, 4.0),
-          Extent(9.0, 2.0, 10.0, 3.0), Extent(9.0, 3.0, 10.0, 4.0))
+      Extent("s", 5, 2, 10, 4).separate(5, 2) must containTheSameElementsAs(
+        Seq(Extent("s-0",5.0,2.0,6.0,3.0),
+          Extent("s-1",5.0,3.0,6.0,4.0),
+          Extent("s-2",6.0,2.0,7.0,3.0),
+          Extent("s-3",6.0,3.0,7.0,4.0),
+          Extent("s-4",7.0,2.0,8.0,3.0),
+          Extent("s-5",7.0,3.0,8.0,4.0),
+          Extent("s-6",8.0,2.0,9.0,3.0),
+          Extent("s-7",8.0,3.0,9.0,4.0),
+          Extent("s-8",9.0,2.0,10.0,3.0),
+          Extent("s-9",9.0,3.0,10.0,4.0))
       )
+
+      Extent("s", 0, 0, 10, 10).separate(2, 2).foreach(println)
+
+      ok
     }
   }
 }
